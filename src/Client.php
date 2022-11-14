30a5a8158e364fcd43512d859640744d20c49245
David Cameron <david.cameron@usda.gov>
2022-11-14 13:00:01 -0500

    Improved error response and logging

--- src/Client.php
+++ src/Client.php
@@ -3,12 +3,16 @@
 namespace Drupal\crossref_api_client;
 
 use Drupal\Component\Utility\NestedArray;
+use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Http\ClientFactory;
 use Drupal\Core\Messenger\Messenger;
 use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ServerException;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
 
 /**
  * Provides methods for connecting to the Crossref API.
@@ -41,6 +45,11 @@ class Client {
   protected $messenger;
 
   /**
+   * @var \Psr\Log\LoggerInterface $logger
+   */
+  protected $logger;
+
+  /**
    * Constructs a new Client object.
    *
    * @param \Drupal\Core\Http\ClientFactory $client_factory
@@ -51,12 +60,15 @@ class Client {
    *   The cache.crossref_responses cache bin.
    * @param \Drupal\Core\Messenger\Messenger $messenger
    *   The Drupal messenger service.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   The logger.channel.crossref_api_client service.
    */
-  public function __construct(ClientFactory $client_factory, ConfigFactory $config_factory, CacheBackendInterface $cache, Messenger $messenger) {
+  public function __construct(ClientFactory $client_factory, ConfigFactory $config_factory, CacheBackendInterface $cache, Messenger $messenger, LoggerInterface $logger) {
     $this->guzzleClient = $client_factory->fromOptions(['base_uri' => self::BASE_URI]);
     $this->config = $config_factory->get('crossref_api_client.settings');
     $this->cache = $cache;
     $this->messenger = $messenger;
+    $this->logger = $logger;
   }
 
   /**
@@ -110,7 +122,28 @@ class Client {
     }
     $options = NestedArray::mergeDeep($options, $extra_options);
 
-    $response = $this->guzzleClient->request($method, $request_path, $options);
+    try {
+      $response = $this->guzzleClient->request($method, $request_path, $options);
+    }
+    catch (ConnectException $e) {
+      $this->messenger->addError('There was a problem while connecting to Crossref. Please try again later. Additional details have been logged.');
+      $this->logger->error('The Crossref API Client module was unable to establish a connection to the API and a Guzzle ConnectException was thrown. The error occurred while connecting to @uri.', [
+        '@uri' => (string) $e->getRequest()->getUri(),
+      ]);
+      throw $e;
+    }
+    catch (ServerException $e) {
+      $this->messenger->addError('Crossref experienced an error while handling the request. Please try again later. Additional details have been logged.');
+      $this->logger->error('The Crossref API returned a 5xx error for a request:<br>
+        Status Code: @status_code<br>
+        URI: @uri<br>
+        Response Message: @message', [
+          '@status_code' => $e->getResponse()->getStatusCode(),
+          '@uri' => (string) $e->getRequest()->getUri(),
+          '@message' => $e->getMessage(),
+        ]);
+      throw $e;
+    }
     $this->cache->set($cid, $response, time() + 86400);
     return $response;
   }
@@ -126,7 +159,7 @@ class Client {
    * @return array
    *   The decoded JSON response from the API.
    *
-   * @throws \GuzzleHttp\Exception\ClientException
+   * @throws \GuzzleHttp\Exception\TransferException
    *   Re-throws the Guzzle exception so that calling code can react to it.
    */
   public function worksDoi(string $doi) {
@@ -134,7 +167,10 @@ class Client {
       $response = $this->request('GET', '/works/{doi}', ['{doi}' => $doi]);
     }
     catch (ClientException $e) {
-      $this->messenger->addWarning('The requested DOI could not be found in Crossref.');
+      $response = $e->getResponse();
+      if ($response->getStatusCode() == 404) {
+        $this->messenger->addWarning('The requested DOI could not be found in Crossref.');
+      }
       throw $e;
     }
     return json_decode($response->getBody()->getContents());
